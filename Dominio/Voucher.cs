b1093ead925b7724a80164fc3dcce203da2da667
PameKid <pamelat.vizcarra@gmail.com>
2025-09-24 13:43:48 -0300

    Clase voucher y voocher negocio

---
+++ Dominio/Voucher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Voucher
+    {
+        public int IdVoucher { get; set; }
+
+        public string CodigoVoucher {  get; set; }
+
+
+        public DateTime DFechaCanje{ get; set; }
+
+
+        public int IDArticulo {  get; set; }
+    }
+}
---
+++ Negocio/VoucherNegocio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class VoucherNegocio
+    {
+        public bool Estado()
+        {
+            return false;
+        }
+
+        public bool consultarVoucherDisponible(string codigoVoucher)
+        {
+            try
+            {
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex ;
+            }
+            
+        }
+    }
+}

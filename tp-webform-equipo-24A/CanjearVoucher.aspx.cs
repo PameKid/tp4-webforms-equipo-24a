89fd37548caa79b3a80ce38391d0a71cbfa11038
PameKid <pamelat.vizcarra@gmail.com>
2025-09-25 16:17:08 -0300

    Conexion con pri

--- tp-webform-equipo-24A/CanjearVoucher.aspx.cs
+++ tp-webform-equipo-24A/CanjearVoucher.aspx.cs
@@ -25,7 +25,7 @@ namespace tp_webform_equipo_24A
 
             if(disponible == true)
             {
-                Response.Redirect("paginasiguientePris");
+                Response.Redirect("SeleccionarProducto.aspx");
             }
 
             else { Response.Redirect("PaginaError.aspx");
